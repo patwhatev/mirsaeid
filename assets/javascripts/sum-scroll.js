1c7a5ebfb1a1e2270016d582deac3cfdc3938f32
Pat Ev <everman.patrick@gmail.com>
2015-06-01 23:21:14 -0400

    lightbox image array and json

--- assets/javascripts/sum-scroll.js
+++ assets/javascripts/sum-scroll.js
@@ -10,42 +10,6 @@
         var $clickIt = $('#clickIt');
         var $lightBoxClose = $('#lightBoxClose');
         var $stickThis = $('.stick-this');
-        var projects = {
-            "sumzine": {
-                "text": "something goes here",
-                "images": ["assets/stylesheets/content/sum/img/2_SUMZINE.jpg", "assets/stylesheets/content/sum/img/3_SUMZINE.jpg", "assets/stylesheets/content/sum/img/4_SUMZINE.jpg" ,        "assets/stylesheets/content/sum/img/4_SUMZINE.jpg", "assets/stylesheets/content/sum/img/5_SUMZINE.jpg"] 
-            },
-            "mirett" : {
-                "text": "assets/stylesheets/content/shims/mirett.html",
-                "images": [ "assets/stylesheets/content/mir/img/2_MIRETT.jpg", "assets/stylesheets/content/mir/img/3_MIRETT.jpg", "assets/stylesheets/content/mir/img/4_MIRETT.jpg", "assets/stylesheets/content/mir/img/5_MIRETT.jpg", "assets/stylesheets/content/mir/img/6_MIRETT.jpg", "assets/stylesheets/content/mir/img/7_MIRETT.jpg", "assets/stylesheets/content/mir/img/8_MIRETT.jpg"] 
-            },
-            "sher1" : {
-                    "text": "assets/stylesheets/content/shims/sherwood.html",
-                    "images": [ "assets/stylesheets/content/sher/sh-1/img/1_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/2_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/3_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/4_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/5_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/6_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/7_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/8_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/9_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/10_SHERWOOD.jpg", "assets/stylesheets/content/sher/sh-1/img/11_SHERWOOD.jpg"] 
-            },
-            "ocblog" : {
-                    "text": "assets/stylesheets/content/shims/ocblog.html",
-                    "images": [ "assets/stylesheets/content/ocb/img/OCBLOG_1.jpg", "assets/stylesheets/content/ocb/img/OCBLOG_2.jpg", "assets/stylesheets/content/ocb/img/OCBLOG_3.jpg"] 
-            }
-        };
-
-        function render(project) { // a key
-            $('#lb-img').html(project.text) //put the response on the dom
-
-            var imageHtml = '';
-            project.images.forEach(function(img) {
-                imageHtml += '<img src="' + img + '" />'
-                console.log(img);
-                // put the image wherever
-            });
-            $('#imageZone').html(imageHtml); //put the response on the dom
-
-        }
-
-        //to swap out an image source, use
-        function swapSrc(img, src) {
-            $(img).attr('src', src);
-        }
         
          //lightbox
         function setKeys() {
@@ -64,7 +28,6 @@
          $clickIt.click(function(e) {
              $lightBox.fadeIn(function() {
                  $lightBoxContent.show();
-                 render(projects[curentProject]);
         
              });
              /* positionLightBox1(); */
@@ -105,31 +68,45 @@
          
          
          //scrolltop change image on HOMEPAGE
-
-        var curentProject;
+         
+        function swapIt (color) {
+            // $('#lightBoxContent').css('background: url("' + path + '"');
+            $('#lightBoxContent').css('border: 1px solid' + color);
+            // $('#lightBoxContent').find('img').attr('src', path);
+            // $('#lightBoxContent').find('a').attr('href', path);
+        }  */ 
          
          $(window).scroll(function() {    
             var scroll = $(window).scrollTop();
             
                 if (scroll < 500){
-                    $("#image-box").removeClass()
+                    $("#image-box").removeClass();
+                    swapIt('red');
                 }
                 if (scroll >= 500) {
                     $("#image-box").removeClass()
-                    $("#image-box").addClass("im-sum")
-                    curentProject = 'sumzine';
+                    $("#image-box").addClass("im-sum");
+                    swapIt('blue');
+
+
                 } 
                 if (scroll >= 1000) {
                     $("#image-box").removeClass()
                     $("#image-box").addClass("im-mir")
+                    swapIt('pink');
+
                 } 
                 if (scroll >= 1500) {
                     $("#image-box").removeClass()
                     $("#image-box").addClass("im-sh1")
+                    swapIt('orange');
+
                 } 
                 if (scroll >= 2000) {
                     $("#image-box").removeClass()
                     $("#image-box").addClass("im-sh2")
+                    swapIt('black');
+
                 } 
                 if (scroll >= 2500) {
                     $("#image-box").removeClass()
